bb7b18cc88a92546615b2bff11bb4dfda9ab1ccc
Howie New <niujiahao1942@outlook.com>
2025-05-01 15:26:46 +0800

    feat: DaemonDemo

---
+++ JUC/src/main/java/dev/howienew/UserDaemonThread/DaemonDemo.java
@@ -0,0 +1,42 @@
+package dev.howienew.UserDaemonThread;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * ClassName: DaemonDemo
+ * Package: dev.howienew.UserDeamonThread
+ * Description:
+ *
+ * @Author: Howie-New
+ * @Create: 2025/5/1 - 15:08
+ */
+public class DaemonDemo {
+
+    public static void main(String[] args) {
+        //Thread t1 is daemon thread
+        Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " start running");
+            System.out.println(Thread.currentThread().getName() + " is " + (Thread.currentThread().isDaemon() ? "Daemon Thread" : "User Thread"));
+            while (true) {
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName() + " is still running");
+            }
+        }, "t1");
+        t1.setDaemon(true);
+        t1.start();
+
+        //Main
+        System.out.println(Thread.currentThread().getName() + " start running");
+        System.out.println(Thread.currentThread().getName() + " is " + (Thread.currentThread().isDaemon() ? "Daemon Thread" : "User Thread"));
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Thread.currentThread().getName() + " end running");
+    }
+}
