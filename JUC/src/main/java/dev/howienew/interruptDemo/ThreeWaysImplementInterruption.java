6824edfd7e0477d89eaaafd177378b19f7c2c8b6
Howie New <niujiahao1942@outlook.com>
2025-05-05 18:04:10 +0800

    feat&Doc: Interrupt and doc fix

---
+++ JUC/src/main/java/dev/howienew/interruptDemo/ThreeWaysImplementInterruption.java
@@ -0,0 +1,95 @@
+package dev.howienew.interruptDemo;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * ClassName: ThreeWaysImplementInterruption
+ * Package: dev.howienew.interruptDemo
+ * Description:
+ *
+ * @Author: Howie-New
+ * @Create: 2025/5/5 - 16:57
+ */
+public class ThreeWaysImplementInterruption {
+    private static volatile boolean interrupted = false;
+    private static AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+    public static void main(String[] args) {
+//        volatileWay();
+//        atomicBooleanWay();
+        interruptWay();
+    }
+
+    private static void interruptWay() {
+        Thread t1 = new Thread(() -> {
+            while (true) {
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println(Thread.currentThread().getName() + " is interrupted!");
+                    break;
+                }
+                System.out.println(Thread.currentThread().getName() + " is still running!");
+            }
+        }, "t1");
+        t1.start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " trying to interrupt t1!");
+            t1.interrupt();
+            System.out.println(Thread.currentThread().getName() + " interrupted t1!");
+        }, "t2").start();
+    }
+
+    private static void atomicBooleanWay() {
+        new Thread(() -> {
+            while (true) {
+                if (atomicBoolean.get()) {
+                    System.out.println(Thread.currentThread().getName() + " is interrupted!");
+                    break;
+                }
+                System.out.println(Thread.currentThread().getName() + " is still running!");
+            }
+        }, "t1").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " trying to interrupt t1!");
+            atomicBoolean.set(true);
+            System.out.println(Thread.currentThread().getName() + " interrupted t1!");
+        }, "t2").start();
+    }
+
+    private static void volatileWay() {
+        new Thread(() -> {
+            while (true) {
+                if (interrupted) {
+                    System.out.println(Thread.currentThread().getName() + " is interrupted!");
+                    break;
+                }
+                System.out.println(Thread.currentThread().getName() + " is still running!");
+            }
+        }, "t1").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " trying to interrupt t1!");
+            interrupted = true;
+            System.out.println(Thread.currentThread().getName() + " interrupted t1!");
+        }, "t2").start();
+    }
+}
--- JUC/src/main/java/dev/howienew/lockdemo/Lock.md
+++ JUC/src/main/java/dev/howienew/lockdemo/Lock.md
@@ -1,10 +1,10 @@
 隐式锁 synchronized
 显式锁 Lock lock = new ReentrantLock();
+所有的对象都可以成为锁，即管程(Monitor)，应为所有对象都继承了Object，而Object对应底层ObjectMonitor.cpp 和 ObjectMonitor.hpp
+![img.png](img.png)
 
+死锁案例和排查命令JVM进程对应的pid
 
-死锁案例和排查命令
-jps -l 找到对应线程对应的线程id
-
-jstack thread id
+jstack pid
 
 或者jconsole等图形化界面
