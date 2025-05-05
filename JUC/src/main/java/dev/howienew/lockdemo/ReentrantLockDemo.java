f1c90786b6c1530a35d30d4c6ea978a338f4c01a
Howie New <niujiahao1942@outlook.com>
2025-05-05 11:57:52 +0800

    feat: ReentrantLockDemo

---
+++ JUC/src/main/java/dev/howienew/lockdemo/ReentrantLockDemo.java
@@ -0,0 +1,51 @@
+package dev.howienew.lockdemo;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * ClassName: ReentrantLockDemo
+ * Package: dev.howienew.lockdemo
+ * Description:
+ *
+ * @Author: Howie-New
+ * @Create: 2025/5/5 - 11:31
+ */
+public class ReentrantLockDemo {
+    private static Lock lock = new ReentrantLock();
+    public static void main(String[] args) {
+        ReentrantLockDemo demo = new ReentrantLockDemo();
+        demo.demo1();
+    }
+
+
+
+    private void demo1() {
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + " get lock and come in");
+                lock.lock();
+                try {
+                    System.out.println(Thread.currentThread().getName() + " get lock and come in");
+                } finally {
+                    System.out.println(Thread.currentThread().getName() + " unlock and quit");
+                    lock.unlock();
+                }
+            } finally {
+                System.out.println(Thread.currentThread().getName() + " unlock and quit");
+                lock.unlock();
+            }
+        }, "t1").start();
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + " get lock and come in");
+            } finally {
+                System.out.println(Thread.currentThread().getName() + " unlock and quit");
+                lock.unlock();
+            }
+        }, "t2").start();
+    }
+}
